bed72a14c75ac3aa8f0536d3e92fc1b8646ac68f
erfannna <erfannaeini2@gmail.com>
2025-07-11 03:20:20 +0330

    third

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export DJANGO_SUPERUSER_USERNAME="test"
+export DJANGO_SUPERUSER_EMAIL="your_email@example.com"
+export DJANGO_SUPERUSER_PASSWORD="test"
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py createsuperuser --no-input
+unset DJANGO_SUPERUSER_USERNAME
+unset DJANGO_SUPERUSER_EMAIL
+unset DJANGO_SUPERUSER_PASSWORD
