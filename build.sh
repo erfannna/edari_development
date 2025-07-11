a51fcb4f6f661a5b30a12e38376a59c278fa1f6f
erfannna <erfannaeini2@gmail.com>
2025-07-11 03:53:19 +0330

    bash

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-export DJANGO_SUPERUSER_PASSWORD="1111"
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py createsuperuser --id_number=1111 --phone_number=1111 --noinput
-
-unset DJANGO_SUPERUSER_PASSWORD
